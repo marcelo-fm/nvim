258118de0e23ea697db3b3f56a8c3791cea74bae
marcelo.mesquita <marcelo.mesquita@codex.com.br>
2023-10-19 09:14:48 -0300

    feat(keymaps.lua): adiciona keymaps para DBUI e Copilot
    Adiciona keymaps para o DBUI e Copilot no arquivo keymaps.lua. Os keymaps adicionados são:
    
    - `<leader>Dt`: Toggle DBUI
    - `<leader>Dc`: Add DBUI Connection
    - `<leader>Df`: Find DBUI Buffer
    - `<leader>fd`: Find DBUI Buffer
    - `<leader>cce`: Enable Copilot
    - `<leader>ccd`: Disable Copilot
    - `<leader>ccp`: Open Copilot Panel

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,41 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>Dt", ":DBUIToggle<CR>", { desc = "Toggle DBUI", noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>Dc",
+  ":DBUIAddConnection<CR>",
+  { desc = "Add DBUI Connection", noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>Df",
+  ":DBUIFindBuffer<CR>",
+  { desc = "Find DBUI Buffer", noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>fd",
+  ":DBUIFindBuffer<CR>",
+  { desc = "Find DBUI Buffer", noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>cce",
+  ":Copilot enable <CR>",
+  { desc = "Enable Copilot", noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ccd",
+  ":Copilot disable <CR>",
+  { desc = "Disable Copilot", noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ccp",
+  ":Copilot panel <CR>",
+  { desc = "Open Copilot Panel", noremap = true, silent = true }
+)
