140d2eecf90e1e2800280310219271e525158cfd
marcelo.mesquita <marcelo.mesquita@codex.com.br>
2023-11-27 20:24:36 -0300

    feat: added harpoons

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,1 +1,45 @@
-return { { "ThePrimeagen/harpoon" } }
+return {
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      {
+        "<leader>hm",
+        function()
+          require("harpoon.mark").add_file()
+        end,
+        desc = "Mark file",
+      },
+      {
+        "<leader>hq",
+        function()
+          require("harpoon.ui").toggle_quick_menu()
+        end,
+        desc = "Quick menu",
+      },
+      {
+        "<leader>fh",
+        function()
+          require("telescope").extensions.harpoon.marks()
+        end,
+        desc = "Find marks",
+      },
+      {
+        "<leader>hn",
+        function()
+          require("harpoon.ui").nav_next()
+        end,
+        desc = "Next mark",
+      },
+      {
+        "<leader>hp",
+        function()
+          require("harpoon.ui").nav_prev()
+        end,
+        desc = "Previous mark",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("harpoon")
+    end,
+  },
+}
