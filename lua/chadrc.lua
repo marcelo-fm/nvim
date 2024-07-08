14943f247347d3be71f3b5641b0785cb1e3f456b
marcelo-fm <marcelofosemesquita@gmail.com>
2024-07-08 10:51:31 -0300

    feat: update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,31 @@
 local M = {}
 
 M.ui = {
+  theme_toggle = { "gruvbox", "gruvbox_light" },
   theme = "gruvbox_light",
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+  },
+  telescope = { style = "borderless" }, -- borderless / bordered
+
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    order = nil,
+    modules = nil,
+  },
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+  },
   nvdash = {
     load_on_startup = true,
 
@@ -46,6 +65,29 @@ M.ui = {
       { "  Themes", "Spc t h", "Telescope themes" },
       { "  Mappings", "Spc c h", "NvCheatsheet" },
     },
+  },
+  term = {
+    winopts = { winhl = "Normal:term,WinSeparator:WinSeparator" },
+    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.3,
+      col = 0.25,
+      width = 0.5,
+      height = 0.4,
+      border = "single",
+    },
+  },
+
+  base46 = {
+    integrations = {},
+  },
+
+  lsp = { signature = true },
+
+  cheatsheet = {
+    theme = "grid", -- simple/grid
+    excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
   },
 }
 
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,37 +1,23 @@
 return {
-  {
-    enabled = false,
-    "stevearc/oil.nvim",
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("oil").setup {
-        -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-        -- Set to false if you still want to use netrw.
-        default_file_explorer = true,
-        keymaps = {
-          ["q"] = "<cmd>q<CR>",
-          ["g?"] = "actions.show_help",
-          ["<CR>"] = "actions.select",
-          ["<C-s>"] = "actions.select_vsplit",
-          ["<C-h>"] = "actions.select_split",
-          ["<C-t>"] = "actions.select_tab",
-          ["<C-p>"] = "actions.preview",
-          ["<C-c>"] = "actions.close",
-          ["<C-l>"] = "actions.refresh",
-          ["<BS>"] = "actions.parent",
-          ["_"] = "actions.open_cwd",
-          ["`"] = "actions.cd",
-          ["~"] = "actions.tcd",
-          ["gs"] = "actions.change_sort",
-          ["gx"] = "actions.open_external",
-          ["g."] = "actions.toggle_hidden",
-          ["g\\"] = "actions.toggle_trash",
-        },
-        -- Set to false to disable all of the above keymaps
-        use_default_keymaps = true,
-      }
-    end,
+  "stevearc/oil.nvim",
+  opts = {
+    default_file_explorer = true,
+    experimental_watch_for_changes = true,
+    delete_to_trash = true,
   },
+  config = function()
+    require("oil").setup {
+      columns = { "icon" },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<M-h>"] = "actions.select_split",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
+    vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+  end,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
