3e6397be671c92b35e3c43abc589b8f868a176bc
marcelo.mesquita <marcelo.mesquita@codex.com.br>
2024-07-08 11:50:34 -0300

    feat: upd

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,14 @@
+-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+--
+-- This is often very useful to both group configuration, as well as handle
+-- lazy loading plugins that don't need to be loaded immediately at startup.
+--
+-- For example, in the following configuration, we use:
+--  event = 'VimEnter'
+
+return { -- Highlight todo, notes, etc in comments
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false },
+}
