0c2d80c631ad793204fe8eac5d787eb536c92d55
marcelo-fm <marcelofosemesquita@gmail.com>
2024-07-08 11:16:44 -0300

    feat: ipd

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,17 @@
+return {
+  'linrongbin16/colorbox.nvim',
+
+  -- don't lazy load
+  lazy = false,
+  -- load with highest priority
+  priority = 1000,
+
+  build = function()
+    require('colorbox').update()
+  end,
+  config = function()
+    require('colorbox').setup {
+      filter = false,
+    }
+  end,
+}
