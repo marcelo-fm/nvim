a2d8a321862134cd666dcb32298e6a134edc0e53
marcelo.mesquita <marcelo.mesquita@codex.com.br>
2024-11-14 17:21:40 -0300

    feat: update

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -23,5 +23,39 @@ return {
         path = "/mnt/c/Users/marcelo.mesquita/Documents/obsidian",
       },
     },
+    notes_subdir = "1 - Notas",
+    log_level = vim.log.levels.DEBUG,
   },
+  completion = {
+    -- Set to false to disable completion.
+    nvim_cmp = true,
+    -- Trigger completion at 2 chars.
+    min_chars = 2,
+  },
+  mappings = {
+    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+    -- Toggle check-boxes.
+    ["<leader>ch"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+    -- Smart action depending on context, either follow link or toggle checkbox.
+    ["<cr>"] = {
+      action = function()
+        return require("obsidian").util.smart_action()
+      end,
+      opts = { buffer = true, expr = true },
+    },
+  },
+  new_notes_location = "notes_subdir",
+  preferred_link_style = "wiki",
+  disable_frontmatter = false,
 }
