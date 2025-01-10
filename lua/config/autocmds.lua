f03e988730326fb6b4d922e627768ea19893d456
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2025-01-10 19:17:05 -0300

    feat: added config to disable copilot

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+  augroup CopilotDisable
+    autocmd!
+    autocmd VimEnter * Copilot disable
+  augroup END
+]])
