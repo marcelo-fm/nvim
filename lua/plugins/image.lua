8d1bd4d378e038eb18d9d92ba6452be1d5113f9e
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2025-04-18 23:13:29 -0300

    update

---
+++ lua/plugins/image.lua
@@ -0,0 +1,67 @@
+return {
+  {
+    "3rd/image.nvim",
+    dependencies = { "luarocks.nvim" },
+    opts = {
+      backend = "kitty",
+      max_width = 100,
+      max_height = 12,
+      max_height_window_percentage = math.huge, -- this is necessary for a good experience
+      max_width_window_percentage = math.huge,
+      window_overlap_clear_enabled = true,
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          floating_windows = false, -- if true, images will be rendered in floating markdown windows
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+        html = {
+          enabled = true,
+        },
+        css = {
+          enabled = true,
+        },
+      },
+    },
+  },
+  {
+    "3rd/diagram.nvim",
+    dependencies = {
+      "3rd/image.nvim",
+    },
+    opts = { -- you can just pass {}, defaults below
+      events = {
+        render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
+        clear_buffer = { "BufLeave" },
+      },
+      renderer_options = {
+        mermaid = {
+          background = nil, -- nil | "transparent" | "white" | "#hex"
+          theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+          scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+          width = nil, -- nil | 800 | 400 | ...
+          height = nil, -- nil | 600 | 300 | ...
+        },
+        plantuml = {
+          charset = nil,
+        },
+        d2 = {
+          theme_id = nil,
+          dark_theme_id = nil,
+          scale = nil,
+          layout = nil,
+          sketch = nil,
+        },
+        gnuplot = {
+          size = nil, -- nil | "800,600" | ...
+          font = nil, -- nil | "Arial,12" | ...
+          theme = nil, -- nil | "light" | "dark" | custom theme string
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/rocks.lua
@@ -0,0 +1,10 @@
+return {
+  "vhyrro/luarocks.nvim",
+  priority = 1000,
+  opts = {
+    rocks = {
+      hererocks = true,
+      magick = true,
+    },
+  },
+}
