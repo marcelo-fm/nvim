829a8192c48e68f28a8a4213990421ababb074ed
Marcelo Mesquita <marcelo.mesquita@codex.com.br>
2025-04-23 22:24:49 -0300

    feat:updated

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -17,7 +17,7 @@ return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
-  cmd = { "ObsidianToday", "Obsidian", "ObsidianNew" }, -- Load plugin when :Obsidian is called.
+  cmd = { "ObsidianToday", "Obsidian", "ObsidianNew", "ObsidianDailies", "ObsidianSearch", "ObsidianTOC" }, -- Load plugin when :Obsidian is called.
   ft = { "markdown" }, -- Lazy-load for Markdown files.
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
