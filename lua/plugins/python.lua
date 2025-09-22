2ca91be307968c09e87daa814122433a2b31956a
Marcelo Mesquita <marcelo.mesquita@codex.com.br>
2025-09-22 10:54:18 -0300

    feat: update

---
+++ lua/plugins/python.lua
@@ -0,0 +1,18 @@
+return {
+  ---@module 'python'
+  {
+    "joshzcold/python.nvim",
+    dependencies = {
+      { "mfussenegger/nvim-dap" },
+      { "mfussenegger/nvim-dap-python" },
+      { "neovim/nvim-lspconfig" },
+      { "L3MON4D3/LuaSnip" },
+      { "nvim-neotest/neotest" },
+      { "nvim-neotest/neotest-python" },
+    },
+    ---@type python.Config
+    opts = { ---@diagnostic disable-line: missing-fields`
+      python_lua_snippets = true,
+    },
+  },
+}
